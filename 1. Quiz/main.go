99a524465771cd79e3e4e2c92d3f4b103b47efde
hija <hilko.j@wwu.de>
2020-08-06 19:27:16 +0200

    Added second part of the task

--- 1. Quiz/main.go
+++ 1. Quiz/main.go
@@ -5,10 +5,10 @@ import (
 	"encoding/csv"
 	"flag"
 	"fmt"
-	"io"
 	"log"
 	"os"
 	"strings"
+	"time"
 )
 
 // We define a struct for the questions
@@ -18,10 +18,16 @@ type questionAnswer struct {
 	Answer   string // This usually is a number, since it is a math quiz. However, we stick to string to keep it more general.
 }
 
+func showScoreAndExit(score int, totalQuestions int) {
+	fmt.Println("You scored", score, "points! There were", totalQuestions, "questions in total!")
+	os.Exit(0)
+}
+
 func main() {
 
 	// Get commandline arguments
 	csvFilename := flag.String("quizfile", "problems.csv", "CSV file containing the questions")
+	timeout := flag.Int("timeout", 30, "Maximum time to answer all questions")
 
 	flag.Parse()
 
@@ -35,24 +41,35 @@ func main() {
 	// We read the file as csv
 	csvReader := csv.NewReader(bufio.NewReader(file))
 
+	// We read in all questions
+	records, err := csvReader.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	// Score of the user
 	score := 0
 
 	// Total questions
-	total_questions := 0
+	totalQuestions := len(records)
 
 	// Input reader for the user's answer
 	stdinReader := bufio.NewReader(os.Stdin)
 
+	// Tell the user the game is about to start
+	fmt.Println("The game starts now! You only have", *timeout, "seconds to answer all question. Once you are ready hit the ENTER key!")
+	fmt.Scanln()
+
+	// We create a timer
+	timer := time.NewTimer(time.Duration(*timeout) * time.Second)
+	// We create a function which shows the score. This function is run in parallel, so the questions are still shown.
+	go func() {
+		<-timer.C
+		showScoreAndExit(score, totalQuestions)
+	}()
+
 	// We go through each entry in csv file
-	for {
-		record, err := csvReader.Read()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			log.Fatal(err)
-		}
+	for _, record := range records {
 
 		// We put the record into a QuestionAnswer element
 		questionAnswer := questionAnswer{
@@ -73,9 +90,6 @@ func main() {
 			score++
 		}
 
-		total_questions++
 	}
-
-	fmt.Println("You scored", score, "points! There were", total_questions, "questions in total!")
-
+	showScoreAndExit(score, totalQuestions)
 }
